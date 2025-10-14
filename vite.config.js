31a848df0c49ba631a6d49b3c3568200199d8b30
Clark Ngo <clarkngo@gmail.com>
2025-10-14 13:37:05 -0700

    fix: repo

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'serve' ? '/' : '/clarkngo.github.io/',
+  base: "/clarkngo.github.io",
   server: {
     open: true,
     port: 3001,
