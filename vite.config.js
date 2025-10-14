515c8118cd981c947743d82aaea8cf6e455c263d
Clark Ngo <clarkngo@gmail.com>
2025-10-14 14:12:17 -0700

    viteSingleFile

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteSingleFile } from 'vite-plugin-singlefile'; 
 
-// https://vite.dev/config/
-export default defineConfig(({ command }) => ({
-  plugins: [react()],
-  base: "/clarkngo.github.io"
-}))
+export default defineConfig({
+  plugins: [
+    // Include the necessary framework plugin
+    react(), 
+    // Add the vite-plugin-singlefile plugin to inline all CSS and JS into index.html
+    viteSingleFile(),
+  ]
+});
